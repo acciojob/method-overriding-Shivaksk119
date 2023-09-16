976b2cf57f80e74e49436dbbe0e1b6b8314b8e76
Shivaksk119 <shivapatelkarru119ksk@gmail.com>
2023-09-16 14:51:30 +0530

    fourth changes

--- src/main/java/com/driver/B.java
+++ src/main/java/com/driver/B.java
@@ -1,6 +1,6 @@
 package com.driver;
 
-public class class_B extends class_A {
+public class B extends A {
 
     @Override
     public String meth() {
--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,7 +2,7 @@ package com.driver;
 
 public class Main {
     public static void main(String[] args) {
-        class_B b = new class_B();
+        B b = new B();
         System.out.println(b.meth());
     }
 }
